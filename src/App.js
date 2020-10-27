0138cd9854ff9f2625e479e53e1c4c7a55bfbccf
Dev Sebastian <devsebastian@gmail.com>
2020-10-27 21:58:04 +0530

    added alt text for images

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <img src={logo} />
+        <img src={logo} alt="Checko"/>
       </div>
       <div className="body">
         <StepProgressBar />
       </div>
       <div className="footer">
-        <a href='https://play.google.com/store/apps/details?id=com.transpacks.Checko1&hl=en_IN&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' /></a>
+        <a href='https://play.google.com/store/apps/details?id=com.transpacks.Checko1&hl=en_IN&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' alt="Checko App" src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' /></a>
       </div>
     </div>
   );
